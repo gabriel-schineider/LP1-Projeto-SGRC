eb0aa2c6ca995abde7f3f8a26516f3e71374cb7e
Datto06 <rafaosbcarvalho@gmail.com>
2025-10-18 00:58:00 -0300

    Pequeno erro na declaracao de nomes, mas testes deu, faltou testar remover so, e caso de ID nao existir

--- x_modelos_teste.c
+++ x_modelos_teste.c
@@ -21,7 +21,6 @@ void CriarRegistro(FILE* arquivo){
     strncpy(nomes[3], "Couro", ESPACO); // guidom
     int arr[] =  {8500, 1, 8, 12}; // preco ; offroad ; pressao_ideal[2]
     PreencheEntrada(&moto,ARR_TIPOS,arr,nomes);
-    ExibeEntrada(moto); //mostra a entrada a ser escrita
 
     ENTRADA_FINAL carro;
     int ARR_TIPOS_1[] = {ID, indice,0, motorizado_ENUM, carro_ENUM}; // ID ; APAGADO ; classe ; subclasse 
@@ -30,8 +29,7 @@ void CriarRegistro(FILE* arquivo){
     strncpy(nomes_1[1], "Cinza", ESPACO); // cor
     strncpy(nomes_1[2], "Gasolina", ESPACO); // combustivel
     int arr_1[] =  {18500, 4, 4, 1, 8, 8, 12, 12}; // preco ; tracao ; portas ; eletrico ; pressao_ideal[4]
-    PreencheEntrada(&carro,ARR_TIPOS_1,arr_1,nomes);
-    ExibeEntrada(carro);
+    PreencheEntrada(&carro,ARR_TIPOS_1,arr_1,nomes_1);
 
     ENTRADA_FINAL barco;
     int ARR_TIPOS_2[] = {ID, indice,0, 0, 2}; // ID ; APAGADO ; classe ; subclasse 
@@ -41,7 +39,7 @@ void CriarRegistro(FILE* arquivo){
     strncpy(nomes_2[2], "Gasolina", ESPACO); // combustivel
     strncpy(nomes_2[3], "Amadeirado", ESPACO); // tipo_casco
     int arr_2[] =  {958500, 250, 320}; // preco ; litragem ; vel_aq_max
-    PreencheEntrada(&barco,ARR_TIPOS_2,arr_2,nomes);
+    PreencheEntrada(&barco,ARR_TIPOS_2,arr_2,nomes_2);
     
     ENTRADA_FINAL helicoptero;
     int ARR_TIPOS_3[] = {ID, indice,0, 0, 3}; // ID ; APAGADO ; classe ; subclasse 
@@ -50,7 +48,7 @@ void CriarRegistro(FILE* arquivo){
     strncpy(nomes_3[1], "Preto", ESPACO); // cor
     strncpy(nomes_3[2], "Gasolina", ESPACO); // combustivel
     int arr_3[] =  {1518500, 6, 6, 6000}; // preco ; pas ; passageiros ; altitude_max
-    PreencheEntrada(&helicoptero,ARR_TIPOS_3,arr_3,nomes);
+    PreencheEntrada(&helicoptero,ARR_TIPOS_3,arr_3,nomes_3);
 
     ENTRADA_FINAL bike;
     int ARR_TIPOS_4[] = {ID,indice, 0, 1, 0}; // ID ; APAGADO ; classe ; subclasse 
@@ -60,7 +58,7 @@ void CriarRegistro(FILE* arquivo){
     strncpy(nomes_4[2], "Vintage", ESPACO); // modelo
     strncpy(nomes_4[3], "Alta", ESPACO); // suspensao
     int arr_4[] =  {1200, 50, 6};  // preco ; diam_roda ; marchas
-    PreencheEntrada(&bike,ARR_TIPOS_4,arr_4,nomes);
+    PreencheEntrada(&bike,ARR_TIPOS_4,arr_4,nomes_4);
 
 
     ENTRADA_FINAL skate;
@@ -71,23 +69,28 @@ void CriarRegistro(FILE* arquivo){
     strncpy(nomes_5[2], "Street", ESPACO);// modelo
     strncpy(nomes_5[3], "Borracha", ESPACO); // tipo_roda
     int arr_5[] =  {1200, 110, 10}; // preco ; comprimento ; diam_roda
-    PreencheEntrada(&skate,ARR_TIPOS_5,arr_5,nomes);
+    PreencheEntrada(&skate,ARR_TIPOS_5,arr_5,nomes_5);
     
     switch (entrada_USER){
         case 0:
             EscreverEntrada(moto,arquivo);
+            ExibeEntrada(moto);
             break;
         case 1:
             EscreverEntrada(carro, arquivo);
+            ExibeEntrada(carro);
             break;
         case 2:
             EscreverEntrada(barco,arquivo);
+            ExibeEntrada(barco);
             break;
         case 3:
             EscreverEntrada(bike,arquivo);
+            ExibeEntrada(moto);
             break;
         case 4:
             EscreverEntrada(skate,arquivo);
+            ExibeEntrada(moto);
             break;
         default:
             printf("Deu errado(provavelmente input errado)\n");
@@ -99,7 +102,7 @@ void CriarRegistro(FILE* arquivo){
 int main ()
 {
     ENTRADA_FINAL registros_teste[6];
-    FILE* arquivo=fopen("C:\\LP1\\concessionaria\\repositorio\\src\\testes.bin","rb+");
+    FILE* arquivo=fopen("C:\\LP1\\concessionaria\\repositorio\\testes.bin","rb+");
 
     // vou criar um loop para add cada registro à uma posição
 
