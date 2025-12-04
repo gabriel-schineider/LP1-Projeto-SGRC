9da00377a6df79b6143f3c8c0e882889e3fd1843
daniel-pb-gh <daniel.pbacc@gmail.com>
2025-12-04 13:53:40 -0300

    uma continuação da função de remoção permanente

--- perm_remove.h
+++ perm_remove.h
@@ -20,7 +20,17 @@ void perm_rm(FILE *f, int index)
 
     Registro aux;
     long int size = sizeof(Registro);
-    fseek(f, (size * (-1)), SEEK_END);
+    fseek(f, 0, SEEK_SET);
+    beginf = ftell(f)
+    if ((size * (-1)) < beginf) // assume que tem pelo menos um registro
+    {
+        printf("ainda não existem registros, para remover um registro, cire um primeiro. \n");
+        return;
+    }
+
+    fseek(f, (size * (-1)), SEEK_END); // pega o ID que possui último 
+    int ID_do_ultimo_indice;
+    fread(&, sizeof(int), 1, f)// guarda o ID do registro de último indice 
     if(fread(&aux, size, 1, f) != 1)
     {
         perror("falha ao copiar ultimo arquivo para o auxiliar");
@@ -37,14 +47,22 @@ void perm_rm(FILE *f, int index)
 
     fseek(f, (size * (-1)), SEEK_END);
     int file_descr = fileno(f); //aparentemente isso é necessário para o parâmetro do ftruncate abaixo, uma chatice.
-    
+
     #ifdef _WIN32   //acho que só vai ser necessário rodar no linux, mas, caso seja necessário, está aí.
         _chsize_s(file_descr, ftell(f));
     #else
         ftruncate(file_descr, ftell(f));
     #endif
 
+    int ID = indice_P_ID(f, index); //pego o id associado ao indice do apagado
+    fseek(f, (((2* sizeof(int)) * (ID -1)) + 4), SEEK_SET); // pulando para o index associado ao id apagado
+    int deleted = NULO;
+    fwrite(&deleted, sizeof(int, 1, f)); // tornando o index inválido.
+
+    fseek(f, (((2* sizeof(int)) * (ID_do_ultimo_indice -1)) + 4), SEEK_SET) // vai na posição do índice associado ao último ID
+    fwrite(&index, sizeof(int); 1, f); // coloca o indice do arquivo que foi removido como o novo indice do último ID.
+
     return;
 }
-    
+
 #endif
